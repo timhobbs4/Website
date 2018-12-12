d86b1c473fd855bb3d77624fcef3ec5d97cecccb
Tim Hobbs <timhobbs4@bigpond.com>
2018-12-12 14:33:07 +1000

    added smooth scrolling

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,38 +1,12 @@
-/*
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth',
-
-        });
-
-    });
-});
-*/
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+    anchor.scrollTop -= 10;
+    anchor.addEventListener('click', function(e) {
         e.preventDefault();
         var path = document.querySelector(this.getAttribute('href'));
-
         path.scrollIntoView({
-        	behavior: 'smooth',
+            behavior: 'smooth',
         });
-
-        });
-
     });
 
-
-$(document).ready(function(){
-        // scroll body to 0px on click
-        $('#newer-posts').click(function () {
-            $('body,html').animate({
-                scrollTop: 0
-            }, 1000);
-            return false;
-        });
 });
 
