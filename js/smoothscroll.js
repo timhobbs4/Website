ce7757cfcf483cc64f9fab049440d6c2b970ef44
Tim Hobbs <timhobbs4@bigpond.com>
2019-02-05 08:12:21 +1000

    SIT BNE Banner Updated

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,8 +1,15 @@
-$('.novLink, .sepLink, #newer-posts').click(function() {
+$('a.sepLink, #newer-posts').click(function() {
     //assign section reference to variable
     var sectionTo = $(this).attr('href');
     //animate scroll 
     $('html, body').animate({
       scrollTop: $(sectionTo).offset().top - 70
+    }, 1000);
+});
+
+$('a.novLink').click(function(){
+    var sectionTo = $(this).attr('href');
+    $('body, html').animate({
+        scrollTop: $(sectionTo).offset().top -105
     }, 1000);
 });
